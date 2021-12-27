8dd75480091cc71a81b9e95a107dfeea42a7f7da
OnkelPony <zkurvenej@outlook.com>
2021-12-27 22:37:05 +0100

    Partialy implement divide

--- calculator_test.go
+++ calculator_test.go
@@ -5,22 +5,27 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func TestAddSubMul(t *testing.T) {
 	t.Parallel()
 	type testCase struct {
+		fn   func(float64, float64) float64
 		a, b float64
 		want float64
 	}
 	tcs := []testCase{
-		{1, 2, 3},
-		{4, 5, 9},
-		{-7, -8, -15},
-		{108, 666, 774},
+		{calculator.Add, 1, 2, 3},
+		{calculator.Add, 4, 5, 9},
+		{calculator.Add, -7, -8, -15},
+		{calculator.Add, 108, 666, 774},
+		{calculator.Subtract, 1, 2, -1},
+		{calculator.Subtract, 8, -2, 10},
+		{calculator.Multiply, 4, 5, 20},
+		{calculator.Multiply, -7, -8, 56},
 	}
 	for _, tc := range tcs {
-		got := calculator.Add(tc.a, tc.b)
+		got := tc.fn(tc.a, tc.b)
 		if tc.want != got {
-			t.Errorf("Add(%.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, got, tc.want)
+			t.Errorf("AddSubMul(%.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, got, tc.want)
 		}
 	}
 }
@@ -42,5 +47,15 @@ func TestMultiply(t *testing.T) {
 	got := calculator.Multiply(a, b)
 	if want != got {
 		t.Errorf("Multiply(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	var want float64 = 3
+	var a, b float64 = 6, 2
+	got := calculator.Divide(a, b)
+	if want != got {
+		t.Errorf("Divide(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
 	}
 }
