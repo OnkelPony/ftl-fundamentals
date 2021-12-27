bb8ddb97d0b39a9601894dd1e4758b4071b9b600
Jiří Martínek <zwo@centrum.cz>
2021-12-27 16:40:53 +0100

    Implemented Add

--- calculator_test.go
+++ calculator_test.go
@@ -7,18 +7,10 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
+	want := float64(4)
+	var a, b float64 = 2, 2
+	got := calculator.Add(a, b)
 	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+		t.Errorf("Add(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
 	}
 }
-
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
