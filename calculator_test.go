489b121f4fa2c3634235e18e241de951a54f76f6
Jiří Martínek <zwo@centrum.cz>
2021-12-27 16:57:55 +0100

    Implemented Subtract and Multiply

--- calculator_test.go
+++ calculator_test.go
@@ -7,10 +7,30 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	want := float64(4)
+	var want float64 = 4
 	var a, b float64 = 2, 2
 	got := calculator.Add(a, b)
 	if want != got {
 		t.Errorf("Add(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 4
+	var a, b float64 = 6, 2
+	got := calculator.Subtract(a, b)
+	if want != got {
+		t.Errorf("Subtract(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 12
+	var a, b float64 = 6, 2
+	got := calculator.Multiply(a, b)
+	if want != got {
+		t.Errorf("Multiply(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
 	}
 }
