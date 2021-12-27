bf910524dbdbad7ee9d3f5f251e68180bbc1797c
Jiří Martínek <zwo@centrum.cz>
2021-12-27 17:14:02 +0100

    Implemented multi test.

--- calculator_test.go
+++ calculator_test.go
@@ -7,11 +7,21 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	var a, b float64 = 2, 2
-	got := calculator.Add(a, b)
-	if want != got {
-		t.Errorf("Add(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	tcs := []testCase{
+		{1, 2, 3},
+		{4, 5, 9},
+		{-7, -8, -15},
+		{108, 666, 774},
+	}
+	for _, tc := range tcs {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, got, tc.want)
+		}
 	}
 }
 
