d0d629964edfee53f2ef1e08671b97113797fc83
Jiří Martínek <zwo@centrum.cz>
2021-12-28 17:59:27 +0100

    Bookstore using struct

--- bookstore/cmd/main.go
+++ bookstore/cmd/main.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var title, author string
-	var count, editionNumber int
-	title = "Ikona"
-	author = "Frederick Forsyth"
-	count = 108
-	editionNumber = 666
-	fmt.Println(title)
-	fmt.Println(author)
-	fmt.Println(count)
-	fmt.Println(editionNumber)
+	type Book struct {
+		Title        string
+		Author       string
+		Count        int
+		SeriesNumber int
+	}
+	b := Book{
+		Title:        "Ikona",
+		Author:       "Frederick Forsyth",
+		Count:        108,
+		SeriesNumber: 1,
+	}
+	fmt.Println(b)
 }
