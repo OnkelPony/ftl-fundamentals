4fa13f79d6e7e16b15cdd094e4efcff16e74aba8
Jiří Martínek <zwo@centrum.cz>
2021-12-28 14:42:54 +0100

    Bookstore started

---
+++ bookstore/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var title, author string
+	var count, editionNumber int
+	title = "Ikona"
+	author = "Frederick Forsyth"
+	count = 108
+	editionNumber = 666
+	fmt.Println(title)
+	fmt.Println(author)
+	fmt.Println(count)
+	fmt.Println(editionNumber)
+}
