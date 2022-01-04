1f194ee2b62981761df18732179a9c786b846bb3
Jiří Martínek <zwo@centrum.cz>
2022-01-04 16:44:59 +0100

    Implemented variadic math functions.

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -5,23 +5,46 @@ import (
 	"math"
 )
 
-func Add(a, b float64) float64 {
-	return a + b
-}
-
-func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("divide by zero")
+func Add(inputs ...float64) float64 {
+	var result float64
+	for _, input := range inputs {
+		result += input
 	}
-	return a / b, nil
+	return result
+}
+
+func Subtract(inputs ...float64) float64 {
+	result := 2 * inputs[0]
+	for _, input := range inputs {
+		result -= input
+	}
+	return result
+}
+
+func Multiply(inputs ...float64) float64 {
+	var result float64 = 1
+	for _, input := range inputs {
+		result *= input
+	}
+	return result
+}
+
+func Divide(inputs ...float64) (float64, error) {
+	result := inputs[0]
+	for i, input := range inputs {
+		if i == 0 {
+			continue
+		}
+		if input == 0 {
+			return 0, errors.New("divide by zero")
+		}
+		result /= input
+	}
+	return result, nil
+
+	//if b == 0 {
+	//}
+	//return a / b, nil
 }
 
 func Sqrt(a float64) (float64, error) {
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -9,33 +9,33 @@ import (
 func TestAddSubMul(t *testing.T) {
 	t.Parallel()
 	type testCase struct {
-		fn   func(float64, float64) float64
-		a, b float64
-		want float64
+		fn      func(...float64) float64
+		a, b, c float64
+		want    float64
 	}
 	tcs := []testCase{
-		{calculator.Add, 1, 2, 3},
-		{calculator.Add, 4, 5, 9},
-		{calculator.Add, -7, -8, -15},
-		{calculator.Add, 108, 666, 774},
-		{calculator.Subtract, 1, 2, -1},
-		{calculator.Subtract, 8, -2, 10},
-		{calculator.Multiply, 4, 5, 20},
-		{calculator.Multiply, -7, -8, 56},
+		{calculator.Add, 1, 2, 3, 6},
+		{calculator.Add, 4, 5, 9, 18},
+		{calculator.Add, -7, -8, -15, -30},
+		{calculator.Add, 108, 666, 774, 1548},
+		{calculator.Subtract, 1, 2, -1, 0},
+		{calculator.Subtract, 8, -2, 1, 9},
+		{calculator.Multiply, 4, 5, 20, 400},
+		{calculator.Multiply, -7, -8, 5, 280},
 	}
 	for _, tc := range tcs {
-		got := tc.fn(tc.a, tc.b)
+		got := tc.fn(tc.a, tc.b, tc.c)
 		if tc.want != got {
-			t.Errorf("AddSubMul(%.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, got, tc.want)
+			t.Errorf("AddSubMul(%.1f, %.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, tc.c, got, tc.want)
 		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	var a, b float64 = 6, 2
-	got := calculator.Subtract(a, b)
+	var want float64 = 13
+	var a, b, c float64 = 6, 2, -9
+	got := calculator.Subtract(a, b, c)
 	if want != got {
 		t.Errorf("Subtract(%.1f, %.1f) = %.1f, want %.1f", a, b, got, want)
 	}
@@ -54,21 +54,22 @@ func TestMultiply(t *testing.T) {
 func TestDivide(t *testing.T) {
 	t.Parallel()
 	tcs := []struct {
-		a, b        float64
+		a, b, c     float64
 		want        float64
 		errExpected bool
 	}{
-		{a: 108, b: 4, want: 27, errExpected: false},
-		{a: 666, b: 0, want: 999, errExpected: true},
+		{a: 108, b: 4, c: 3, want: 9, errExpected: false},
+		{a: 666, b: 0, c: 8, want: 999, errExpected: true},
+		{666, 66.6, 0, 999, true},
 	}
 	for _, tc := range tcs {
-		got, err := calculator.Divide(tc.a, tc.b)
+		got, err := calculator.Divide(tc.a, tc.b, tc.c)
 		errReceived := err != nil
 		if errReceived != tc.errExpected {
-			t.Fatalf("\"Divide(%.1f, %.1f): unexpected error status: %v", tc.a, tc.b, err)
+			t.Fatalf("\"Divide(%.1f, %.1f, %.1f): unexpected error status: %v", tc.a, tc.b, tc.c, err)
 		}
 		if !errReceived && tc.want != got {
-			t.Errorf("Divide(%.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, got, tc.want)
+			t.Errorf("Divide(%.1f, %.1f, %.1f) = %.1f, want %.1f", tc.a, tc.b, tc.c, got, tc.want)
 		}
 	}
 }
